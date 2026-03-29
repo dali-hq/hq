8eb087b535bb4ff0431cfec12f29ab8ee9b35f2e
Mireille <virtuekyoko@gmail.com>
2026-03-29 20:19:51 +0800

    Create card_data.js

---
+++ card_data.js
@@ -0,0 +1,6 @@
+// card_data.js
+let MY_CARD_DATA = [
+    { id:0, name:"現金(早餐)", cls:"cash", s:1, d:31, type:"cash", details:[], paid: false },
+    { id:1, name:"中信", cls:"ctbc", s:22, d:8, details:[], paid: false },
+    // ... 依此類推
+];
