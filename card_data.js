dfeeb4d735b9e187adaba9cff3d43bbd0e4dd8d3
Mireille <virtuekyoko@gmail.com>
2026-03-29 20:21:31 +0800

    Update card_data.js

--- card_data.js
+++ card_data.js
@@ -4,3 +4,9 @@ let MY_CARD_DATA = [
     { id:1, name:"中信", cls:"ctbc", s:22, d:8, details:[], paid: false },
     // ... 依此類推
 ];
+const MY_CARD_DATA = [
+  { "id": 0, "name": "現金(早餐)", "cls": "cash", "s": 1, "d": 31, "type": "cash", "details": [], "paid": false },
+  { "id": 1, "name": "中信", "cls": "ctbc", "s": 22, "d": 8, "details": [], "paid": false },
+  // ... 剩下的卡片資料
+];
+
